e687c4bc11825dd6f127f245a356d7d5e492d10c
Phillip Friedrich <friedrich.phillip@gmail.com>
2014-01-30 09:26:27 +0100

    sixth

--- src/main/java/com/phillip/news/repository/LanguageRepository.java
+++ src/main/java/com/phillip/news/repository/LanguageRepository.java
@@ -2,8 +2,10 @@ package com.phillip.news.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.phillip.news.domain.Language;
 
 
-/*public interface LanguageRepository extends JpaRepository<Language, Long>{
+
+public interface LanguageRepository extends JpaRepository<Language, Long>{
 	Language findByCode(String code);
-}*/
+}
